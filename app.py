e0dcb88418fd10ffbc356561730a688cb3f1ee0c
swotboysandy <swotboysandy@gmail.com>
2025-04-24 18:57:17 +0530

    For production

--- app.py
+++ app.py
@@ -80,6 +80,7 @@ def play_next_song():
 
 
 if __name__ == '__main__':
-    os.makedirs("templates", exist_ok=True)
-    os.makedirs("static", exist_ok=True)
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
+
